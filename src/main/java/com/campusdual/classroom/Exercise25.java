483233e0de46ff08c6012204375a473cacc57252
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-22 09:02:42 +0100

    Ejercicio 25 completado

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,10 +4,23 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+
+        return stack;
 
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+
+        // Imprimir respetando el orden LIFO
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            System.out.println(stack.get(i));
+        }
 
     }
 
